1b46887e7f5d3ac61a0debf9b008c59dfc336a01
ybw0014 <i@guizhanss.com>
2023-12-31 12:47:44 -0500

    feat: add airtable config

---
+++ @core/cms/src/external-auth-integration/auth-providers/oauth2/airtable/v1/airtable.v1.config.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common";
+import { IOAuth2Config } from "~/external-auth-integration/auth-providers/oauth2/@types";
+
+/**
+ * Service for configuring Airtable V1 OAuth2.
+ */
+@Injectable()
+export class AirtableV1Config {
+  public oAuth2Config: IOAuth2Config;
+
+  constructor() {
+    this.oAuth2Config = {
+      credentials: {
+        id: process.env.AIRTABLE_OAUTH_V1_CLIENT_ID,
+        secret: process.env.AIRTABLE_OAUTH_V1_CLIENT_SECRET,
+      },
+      scope: [], // TODO: Airtable scopes
+      provider: {
+        authorizeUrl: "https://airtable.com/oauth2/v1/authorize",
+        tokenUrl: "https://airtable.com/oauth2/v1/token",
+        callbackUrl: `${process.env.HOST_CONFIG_URL}/oauth/callback`,
+        verifyTokenUrl: "https://airtable.com/oauth2/v1/token",
+        refreshTokenUrl: "https://airtable.com/oauth2/v1/token",
+        tokenRefreshBuffer: 5184000, // 60 days
+        callbackUrlParams: {
+          response_type: "code",
+        },
+      },
+    };
+  }
+}
