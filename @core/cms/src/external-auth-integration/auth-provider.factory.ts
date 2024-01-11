4666f07c7ac87a57b20531657e0b244888b068c4
your_username <pad128g@icloud.com>
2024-01-10 21:49:35 -0500

    fix: add comments of factory

--- @core/cms/src/external-auth-integration/auth-provider.factory.ts
+++ @core/cms/src/external-auth-integration/auth-provider.factory.ts
@@ -10,6 +10,7 @@ import { IBasicAuth } from "./auth-providers/basic-auth/interface/basic-auth.int
 
 @Injectable()
 export class AuthProviderFactory {
+  // A map to hold the associations between authentication keys and their corresponding services
   private readonly providerMap = new Map<AuthString, AuthService>();
 
   constructor(
@@ -17,23 +18,30 @@ export class AuthProviderFactory {
     private githubV1OAuth2Service: GithubV1OAuth2Service,
     private mixpanelV1BasicAuthService: MixpanelV1BasicAuthService,
   ) {
+    // Register available authentication providers
     this.registerProvider("oauth-google-v2", this.googleV2OAuth2Service);
     this.registerProvider("oauth-github-v1", this.githubV1OAuth2Service);
     this.registerProvider("basic-mixpanel-v1", this.mixpanelV1BasicAuthService);
   }
 
+  // Function overloading for registerProvider to handle different types of auth providers
   private registerProvider(key: OAuthString, provider: IOAuth): void;
   private registerProvider(key: BasicAuthString, provider: IBasicAuth): void;
   private registerProvider(key: AuthString, provider: AuthService): void {
+    // Register a provider with a specific key in the map
     this.providerMap.set(key, provider);
   }
 
+  // Function overloading for getProvider to retrieve the appropriate type of auth provider
   public getProvider(key: OAuthString): IOAuth;
   public getProvider(key: BasicAuthString): IBasicAuth;
   public getProvider(key: AuthString): AuthService;
+
   public getProvider(key: AuthString): AuthService {
+    // Retrieve the provider associated with the given key
     const provider = this.providerMap.get(key);
     if (!provider) {
+      // If the provider is not found, throw an error
       throw new Error(`Authentication provider not found for key: ${key}`);
     }
     return provider;
