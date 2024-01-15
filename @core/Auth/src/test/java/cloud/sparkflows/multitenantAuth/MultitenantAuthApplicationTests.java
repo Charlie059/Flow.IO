73b3283070503856aaf1252c07be2d8c38a457e0
your_username <pad128g@icloud.com>
2024-01-14 22:52:30 -0500

    feat: init spring

---
+++ @core/Auth/src/test/java/cloud/sparkflows/multitenantAuth/MultitenantAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.sparkflows.multitenantAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
