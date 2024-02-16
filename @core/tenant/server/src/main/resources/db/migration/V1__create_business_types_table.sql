529bd02d31005330d7a2721506123562ce335687
ybw0014 <i@guizhanss.com>
2024-02-16 15:07:22 -0500

    refactor(migration): add foreign key constraint

--- @core/tenant/server/src/main/resources/db/migration/V1__create_business_types_table.sql
+++ @core/tenant/server/src/main/resources/db/migration/V1__create_business_types_table.sql
@@ -1,3 +1,11 @@
+CREATE TABLE IF NOT EXISTS business_types
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 INSERT INTO business_types (name)
 VALUES ('Sole Proprietorship'),
        ('General Partnership'),
