b3123dc2ca816ec14f329eadce9a46134c44ec7d
ybw0014 <i@guizhanss.com>
2024-04-04 17:21:57 -0400

    chore: format code

--- @core/tenant/server/src/main/java/org/flowio/tenant/controller/RoleController.java
+++ @core/tenant/server/src/main/java/org/flowio/tenant/controller/RoleController.java
@@ -1,16 +1,13 @@
 package org.flowio.tenant.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.flowio.tenant.dto.BusinessTypeDto;
 import org.flowio.tenant.entity.Response;
 import org.flowio.tenant.entity.enums.Role;
-import org.flowio.tenant.service.BusinessTypeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
--- @core/tenant/server/src/main/resources/db/migration/V3__create_users_table.sql
+++ @core/tenant/server/src/main/resources/db/migration/V3__create_users_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users
     email      VARCHAR(255) NOT NULL,
     name       VARCHAR(255) NOT NULL,
     password   VARCHAR(255) NOT NULL,
-    role       VARCHAR(255) NOT NULL,
+    roles      VARCHAR(255) NOT NULL,
     tenant_id  BIGINT       NOT NULL,
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
