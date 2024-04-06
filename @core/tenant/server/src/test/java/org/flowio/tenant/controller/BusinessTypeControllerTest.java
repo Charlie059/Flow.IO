2a5b4b2b5499266e051aac3b0229bfdc7bff1843
ybw0014 <i@guizhanss.com>
2024-04-05 22:58:54 -0400

    fix(test): fix tests with MockMvc
    reference: https://grpc-ecosystem.github.io/grpc-spring/en/trouble-shooting.html#server-port-already-in-use

--- @core/tenant/server/src/test/java/org/flowio/tenant/controller/BusinessTypeControllerTest.java
+++ @core/tenant/server/src/test/java/org/flowio/tenant/controller/BusinessTypeControllerTest.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@DirtiesContext
 class BusinessTypeControllerTest {
     @Autowired
     private MockMvc mvc;
